253db78655b40c386c948cbfe09f964b635b44ae
shubharora16 <shubharora16@gmail.com>
2018-04-22 18:10:28 +0530

    Update TwitterDataExtract.py

--- TwitterDataExtract.py
+++ TwitterDataExtract.py
@@ -4,10 +4,10 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 
 
-access_token = "795235645048246273-CnpWvTLYOLgwrPcJK28OvXxMcL2EJSg"
-access_token_secret = "q3itIa19sNZWXwqF4st5YKL87JnFYIgWy0HuvO0qg7LND"
-consumer_key = "KlHPE7a0xMZVPRo5Yu8SwhPUT"
-consumer_secret = "CeqbdVewkFdzdI0OcGZSwp06PSa9fn7atpddcCupJNu0Pt1CoG"
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
 
 
 
